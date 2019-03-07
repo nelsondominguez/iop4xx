a61248fbe376b79b3003c09dff53aa535a3eeca9
nelsondominguez <nelsonjr73090@gmail.com>
2019-03-07 02:37:29 -0500

    Created the appData object, moved the title & tagline variables into appData and refractored the initializeApplication function.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,18 @@
 // JavaScript Document
 
+// JavaScript document
 'use strict';
 
-var applicationTitle = 'inside out - js';
-
-var applicationTagLine = 'continuously falling forward into the light...';
+var appData = {
+    title: 'inside out - js',
+    tagLine: 'continuously falling forward into the light...'
+};
 
 window.addEventListener('load', initializeApplication);
 
 function initializeApplication() {
 
-    document.title = applicationTitle;
+    document.title = appData.title;
 
     document.body.style.backgroundColor = '#BBB';
     document.body.style.textAlign = 'center';
@@ -24,25 +26,26 @@ function initializeApplication() {
     elWrapper.appendChild(elHeader);
 
     var elHeaderTitle = document.createElement('h1');
-    elHeaderTitle.innerHTML = applicationTitle;
+    elHeaderTitle.innerHTML = appData.title;
     elHeader.appendChild(elHeaderTitle);
+    elHeaderTitle.className = 'animated bounceInDown';
 
     var elHeaderTagLine = document.createElement('h2');
-    elHeaderTagLine.textContent = applicationTagLine;
+    elHeaderTagLine.textContent = appData.tagLine;
     elHeaderTagLine.style.fontSize = '.4em';
     elHeader.appendChild(elHeaderTagLine);
+    elHeaderTagLine.className = 'animated bounceInLeft';
 
     var elMain = document.createElement('main');
     elMain.innerHTML = '<p>Hello World!</p>';
     elWrapper.appendChild(elMain);
+    elMain.className = 'animated zoomIn';
 
     var elFooter = document.createElement('footer');
     elFooter.innerHTML = '<h4>get excited about learning JavaScript...</h4>';
     elWrapper.appendChild(elFooter);
-
-    elHeaderTitle.className = 'animated bounceInDown';
-    elHeaderTagLine.className = 'animated bounceInLeft';
-    elMain.className = 'animated zoomIn';
     elFooter.className = 'animated bounceInRight';
+
+
 
 }
